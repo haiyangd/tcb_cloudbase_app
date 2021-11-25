98e4ee655604bb1fd18575fb20284bee74d6d3b5
haiyangd <donghy2007@hotmail.com>
2021-11-25 15:17:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
     console.log('Hello world received a request.');
 
-    res.send(`Hello, Welcome to CloudBase Enterprise at 16:33!  ENV: ${process.env.ENV_VAR1}`);
+    res.send(`Hello, Welcome to CloudBase!`);
 });
 
 const port = process.env.PORT || 80;
